7a999a7e56f1598d5d589d25318c05e933ef1a77
alireenn <alice.novelli@outlook.com>
2022-04-12 15:03:04 +0200

    Emergency backup

--- so_long/utils/key_manager.c
+++ so_long/utils/key_manager.c
@@ -6,7 +6,7 @@
 /*   By: anovelli <anovelli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 12:58:23 by anovelli          #+#    #+#             */
-/*   Updated: 2022/04/12 14:58:34 by anovelli         ###   ########.fr       */
+/*   Updated: 2022/04/12 15:03:03 by anovelli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ int	command(int key, t_map *map)
 		mlx_destroy_window(map->win.mlx, map->win.win);
 		free_matrix(map->mat);
 	}
-		
-	return (1);
+	return (0);
 }
