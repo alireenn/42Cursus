f2057e923d96354fdd8f5dca7861bf5ffa8f5be8
alireenn <alice.novelli@outlook.com>
2022-05-10 09:33:55 +0200

    Emergency backup

--- push_swap/rules/rules.c
+++ push_swap/rules/rules.c
@@ -6,7 +6,7 @@
 /*   By: anovelli <anovelli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 15:10:14 by anovelli          #+#    #+#             */
-/*   Updated: 2022/05/10 09:18:54 by anovelli         ###   ########.fr       */
+/*   Updated: 2022/05/10 09:33:28 by anovelli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,26 @@
 
 void	sa(t_stacks *stack_a)
 {
-	printf("ciao1");
 	t_node	*temp;
 
-	temp = stack_a->a;
-	temp->next = (stack_a->a->next)->next;
-	stack_a->a = stack_a->a->next;
-	stack_a->a->next = temp;
+	temp = stack_a->a->head;
+	printf("ciao\n");
+	stack_a->a->head = (stack_a->a->head)->next;
 
-	temp = stack_a->a;
-	while (stack_a)
-	{
-		stack_a->a->head = temp;
-		stack_a->a = stack_a->a->next;
-	}
+	printf("ciao2\n");
+	(stack_a->a->head)->next = temp; //qua
+
+	printf("ciao3\n");
+	// temp = stack_a->a;
+	// temp->next = (stack_a->a->next)->next; //qua
+	// stack_a->a = stack_a->a->next;
+	// stack_a->a->next = temp;
+	// temp = stack_a->a;
+	// while (stack_a)
+	// {
+	// 	stack_a->a->head = temp;
+	// 	stack_a->a = stack_a->a->next;
+	// }
 }
 
 
