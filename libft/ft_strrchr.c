e04fa983b192d288d58827790d1760ccba1c5fe9
alireenn <alice.novelli@outlook.com>
2022-05-24 17:28:38 +0200

    Emergency backup

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,24 +6,24 @@
 /*   By: anovelli <anovelli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 18:22:10 by anovelli          #+#    #+#             */
-/*   Updated: 2022/02/01 10:12:00 by anovelli         ###   ########.fr       */
+/*   Updated: 2022/05/24 11:44:33 by anovelli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-char    *ft_strrchr(const char *str, int c)
+char	*ft_strrchr(const char *str, int c)
 {
-    int    i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-        i++;
-    while (i >= 0)
-    {
-        if (str[i] == (char)c)
-            return ((char *)(str + i));
-        i--;
-    }
-    return (NULL);
+	i = 0;
+	while (str[i])
+		i++;
+	while (i >= 0)
+	{
+		if (str[i] == (char)c)
+			return ((char *)(str + i));
+		i--;
+	}
+	return (NULL);
 }
